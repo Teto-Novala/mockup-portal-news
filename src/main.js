5cfd8b809aaa9c5386aea17e6a967a3df9dfe31f
Teto-Novala <tetocoding@gmail.com>
2024-11-24 11:43:03 +0700

    feat: add font poppins and ibm plex serif

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import "./assets/main.css";
 // Supports weights 200-900
 import "@fontsource-variable/nunito-sans/full.css";
+import "@fontsource/poppins";
+import "@fontsource/ibm-plex-serif";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
