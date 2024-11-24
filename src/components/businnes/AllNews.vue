b40a9c86fdc536b26c546c8ba0f18c33acc7f409
Teto-Novala <tetocoding@gmail.com>
2024-11-24 14:50:48 +0700

    feat: add businnes view

---
+++ src/components/businnes/AllNews.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="pt-4 flex flex-col gap-y-4 items-center">
+    <NewsCard
+      v-for="(list, index) in store.dataFormated"
+      :key="index"
+      :img-src="list.urlToImage"
+      :title="list.title"
+      :desc="list.description"
+      :publish="list.publishedAt"
+      :author="list.author"
+    />
+  </section>
+</template>
+
+<script setup>
+import { useAllNewsStore } from "@/stores/allNewsStore";
+import NewsCard from "./NewsCard.vue";
+
+const store = useAllNewsStore();
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/businnes",
+      name: "businnes",
+      component: () => import("../views/BusinnesView.vue"),
     },
   ],
 });
