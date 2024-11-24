f9edb3d2001ad1ab3fe69542a4a589f431e55e5c
Teto-Novala <tetocoding@gmail.com>
2024-11-24 14:20:41 +0700

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer
+    class="bg-[#0E1E32] font-poppins mt-16 flex items-center justify-evenly text-white py-3 text-sm"
+  >
+    <div class="flex flex-col justify-center items-center gap-y-3">
+      <img
+        src="/src/assets/images/footer/logo-footer.svg"
+        alt="logo"
+        class="w-1/3"
+      />
+      <p class="text-slate-300 text-xs">copyright &#169; 2020 | NBC NEWS</p>
+    </div>
+    <div class="flex flex-col gap-y-2">
+      <p>Privacy Policy</p>
+      <p>Do not sell my personal info</p>
+      <p>Terms of Servicenbcnews.com</p>
+      <p>Site Map</p>
+    </div>
+    <div class="flex flex-col gap-y-3 items-end">
+      <ul class="flex items-center gap-x-3">
+        <li>About</li>
+        <li>Contact</li>
+        <li>Carrers</li>
+        <li>Coupons</li>
+      </ul>
+      <div class="flex items-center gap-x-6">
+        <img
+          src="/src/assets/images/footer/wifi.svg"
+          alt="wifi"
+        />
+        <img
+          src="/src/assets/images/footer/twitter.svg"
+          alt="twitter"
+        />
+        <img
+          src="/src/assets/images/footer/robot.svg"
+          alt="robot"
+        />
+        <img
+          src="/src/assets/images/footer/fb.svg"
+          alt="fb"
+        />
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup></script>
