a242627ea3fcb943072c9b277c8b4f9180ceea8a
Teto-Novala <tetocoding@gmail.com>
2024-11-25 03:29:41 +0700

    feat: fix trending component

--- src/components/home/Trending.vue
+++ src/components/home/Trending.vue
@@ -1,24 +1,27 @@
 <template>
-  <section class="flex items-center h-96 gap-x-16">
-    <img
-      :src="`/src/assets/images/home/trending/${store.data.image}`"
-      :alt="store.data.title"
-      class="w-1/2 object-cover object-center"
-    />
-    <div class="w-1/2 font-poppins flex flex-col gap-y-4">
-      <h1 class="text-red-500 font-semibold">Trending</h1>
-      <h2 class="font-ibm font-bold text-xl">{{ store.data.title }}</h2>
-      <p>{{ store.data.description }}</p>
-      <div class="flex items-center gap-x-5 text-sm">
-        <p>{{ store.formattedPublishedAt }}</p>
-        <p>by {{ store.data.author }}</p>
+  <RouterLink to="/businnes/0">
+    <section class="flex items-center h-96 gap-x-16">
+      <img
+        :src="`/src/assets/images/home/trending/${store.data.image}`"
+        :alt="store.data.title"
+        class="w-1/2 object-cover object-center"
+      />
+      <div class="w-1/2 font-poppins flex flex-col gap-y-4">
+        <h1 class="text-red-500 font-semibold">Trending</h1>
+        <h2 class="font-ibm font-bold text-xl">{{ store.data.title }}</h2>
+        <p>{{ store.data.description }}</p>
+        <div class="flex items-center gap-x-5 text-sm">
+          <p>{{ store.formattedPublishedAt }}</p>
+          <p>by {{ store.data.author }}</p>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </RouterLink>
 </template>
 
 <script setup>
 import { useTrendingStore } from "@/stores/trendingStore";
+import { RouterLink } from "vue-router";
 
 const store = useTrendingStore();
 </script>
